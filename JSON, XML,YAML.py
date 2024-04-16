eb30451168e003fefcdd2788fa271c52049c09d6
Pavel <pavelplyakhin@yandex.ru>
2024-04-16 17:49:21 +0500

    16.4.2024

--- JSON, XML,YAML.py
+++ JSON, XML,YAML.py
@@ -6,7 +6,6 @@ def employees_rewrite(sort_type):
     with open('employees.json', 'r') as file:
         json_data = json.load(file)  # -> dict
 
-
         sort_type = sort_type.lower()
         pattern = 'n'
         if sort_type == 'lastname' or sort_type == 'firstname':
@@ -16,7 +15,7 @@ def employees_rewrite(sort_type):
         if type(json_data['employees'][0][sort_type]) is int:
             sort_data = sorted(json_data['employees'], key=lambda x: x[sort_type], reverse=True)
         elif sort_type not in json_data['employees'][0]:
-            raise ValueError('key not found')
+            raise ValueError('Bad key for sorting')
 
         with open(f'employees_{sort_type}_sorted.json', 'w') as out_file:
             json.dump(sort_data, out_file, indent=2)
